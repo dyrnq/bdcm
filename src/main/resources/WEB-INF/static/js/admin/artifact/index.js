a5c9a213090a479a0ae75096c2ee55145cfe13a9
dyrnq <dyrnq@qq.com>
2025-03-12 14:02:45 +0800

    修改artifact页面展示lock的状态

--- src/main/resources/WEB-INF/static/js/admin/artifact/index.js
+++ src/main/resources/WEB-INF/static/js/admin/artifact/index.js
@@ -134,7 +134,7 @@ $('#addOver').click(function(){
             , {field: 'name', title: 'name', width: 200}
             , {field: 'url', title: 'url', width: 300, sort: true}
             , {field: 'autoJob', title: 'autoJob' }
-            , {field: 'lock', title: '_lock' }
+            , {field: 'lock', title: '_lock', templet: "<div>{{d.lock === 1 ? '锁' : ''}}</div>" }
             , {field: 'currentJobId', title: 'currentJobId' }
             , {field: 'insertTime', title: 'insert_time', sort: true, width: 300, templet: "<div>{{!d.insertTime?'-':layui.util.toDateString(d.insertTime, 'yyyy-MM-dd HH:mm:ss') }}</div>" }
             , {field: 'updateTime', title: 'update_time', sort: true, width: 300, templet: "<div>{{!d.updateTime?'-':layui.util.toDateString(d.updateTime, 'yyyy-MM-dd HH:mm:ss') }}</div>" }
