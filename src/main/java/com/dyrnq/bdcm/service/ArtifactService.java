58db3b2152ffdc990825843ee3908fb8f20f2cb1
dyrnq <dyrnq@qq.com>
2025-03-19 15:30:07 +0800

    修正使用HEAD方法无法Content-Length和ETag的问题

--- src/main/java/com/dyrnq/bdcm/service/ArtifactService.java
+++ src/main/java/com/dyrnq/bdcm/service/ArtifactService.java
@@ -409,6 +409,7 @@ public class ArtifactService {
         OkHttpClient client = createOkHttpClient(proxy);
         Request request = new Request.Builder()
                 .url(fileURL)
+                .addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
                 .head() // 使用 HEAD 请求获取文件大小
                 .build();
         Response response = null;
