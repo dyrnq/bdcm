845b6c2ef1c91dd48592ef8ea83188e068b58784
dyrnq <dyrnq@qq.com>
2025-07-28 12:19:15 +0800

    升级layui版本为v2.11.5

--- scripts/bump-layui.sh
+++ scripts/bump-layui.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd -P)
 CODE_BASE="${SCRIPT_DIR}/.."
-ver="v2.11.4"
+ver="v2.11.5"
 curl -O -fSL -# https://github.com/layui/layui/releases/download/"${ver}"/layui-"${ver}".zip
 unzip -u -o layui-"${ver}".zip
 ls -l ./layui-${ver}/layui/
