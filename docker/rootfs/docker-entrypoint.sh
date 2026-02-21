5cae292657c66ea0343735b7c034aac5f353761d
dyrnq <dyrnq@qq.com>
2026-02-21 10:27:07 +0800

    Amend docker/rootfs/docker-entrypoint.sh

--- docker/rootfs/docker-entrypoint.sh
+++ docker/rootfs/docker-entrypoint.sh
@@ -4,6 +4,12 @@ set -eo pipefail
 
 
 _main() {
+
+if [ -n "$TZ" ]; then
+    ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime || true
+    (dpkg-reconfigure --frontend noninteractive tzdata >/dev/null 2>&1 || echo ${TZ} > /etc/timezone ) || true
+fi
+
 touch /etc/s6-overlay/s6-rc.d/user/contents.d/bdcm
 exec /init
 }
